d3c68c4d02bc6eb66e5d7d2742c58a1e8050bc4d
nandinidas07 <nandinidas107@gmail.com>
2025-07-01 22:54:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+it('opens and closes modal', () => {
+  cy.visit('index.html'); // or wherever your modal page is hosted
+  cy.get('#openModal').click();
+  cy.get('.modal').should('have.css', 'display', 'block');
+
+  cy.get('.close-modal').click();
+  cy.get('.modal').should('have.css', 'display', 'none');
+});
